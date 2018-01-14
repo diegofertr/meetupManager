fab58ea4c9885f199ce1aadd8f97caa76cc0afdf
diegofertr <diegonano09@gmail.com>
2018-01-13 22:46:41 -0400

    Añadiendo carousel de meetups a la vista

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,54 @@
-<template lang="html">
-  <div>
-    <p> The Home page</p>
-  </div>
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :src="meetup.imageUrl"
+            :key="meetup.id">
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesomes meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      meetups: [
+        {imageUrl: 'https://static.pexels.com/photos/50632/pexels-photo-50632.jpeg', id: 'hahcq7123714237213', title: 'Meetup in London'},
+        {imageUrl: 'https://c1.staticflickr.com/1/101/268129214_67d7615ff8_b.jpg', id: 'askdjas4645d213', title: 'Meetup in Paris'}
+      ]
+    }
+  }
 }
 </script>
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
