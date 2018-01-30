1fe10a99d37bd3f1ddbbeb8dcc1ebff6c09c625f
Diego Fernando Ticona Ramos <diegonano09@gmail.com>
2018-01-30 19:44:56 -0400

    - Reordenando codigo en el store de Vuex; - Completado el registro de Meetups

---
+++ src/store/meetup/index.js
@@ -0,0 +1,149 @@
+import * as firebase from 'firebase'
+
+export default {
+  state: {
+    loadedMeetups: [
+      {
+        imageUrl: 'https://static.pexels.com/photos/50632/pexels-photo-50632.jpeg',
+        id: 'hahcq7123714237213',
+        title: 'Meetup in London',
+        date: new Date(),
+        location: 'London',
+        description: 'In london the tenth meeting of technology in favor of the fight against crime will be held'
+      },
+      {
+        imageUrl: 'https://c1.staticflickr.com/1/101/268129214_67d7615ff8_b.jpg',
+        id: 'askdjas4645d213',
+        title: 'Meetup in Paris',
+        date: new Date(),
+        location: 'Paris',
+        description: 'It\'s Paris!!'
+      }
+    ]
+  },
+  mutations: {
+    setLoadedMeetups (state, payload) {
+      state.loadedMeetups = payload
+    },
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    },
+    updateMeetup (state, payload) {
+      const meetup = state.loadedMeetups.find(meetup => {
+        return meetup.id === payload.id
+      })
+      if (payload.title) {
+        meetup.title = payload.title
+      }
+      if (payload.description) {
+        meetup.description = payload.description
+      }
+      if (payload.date) {
+        meetup.date = payload.date
+      }
+    }
+  },
+  actions: {
+    loadMeetups ({commit, getters}) {
+      commit('setLoading', true)
+      firebase.database().ref('meetups').once('value')
+        .then((data) => {
+          const meetups = []
+          const obj = data.val()
+          for (let key in obj) {
+            meetups.push({
+              id: key,
+              title: obj[key].title,
+              description: obj[key].description,
+              location: obj[key].location,
+              imageUrl: obj[key].imageUrl,
+              date: obj[key].date,
+              creatorId: obj[key].creatorId
+            })
+          }
+          commit('setLoadedMeetups', meetups)
+          commit('setLoading', false)
+        })
+        .catch(
+          (err) => {
+            console.log(err)
+            commit('setLoading', false)
+          }
+        )
+    },
+    createMeetup ({commit, getters}, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        description: payload.description,
+        date: payload.date.toISOString(),
+        creatorId: getters.user.id
+      }
+      let imageUrl
+      let key
+      firebase.database().ref('meetups').push(meetup)
+        .then((data) => {
+          key = data.key
+          return key
+        })
+        .then(key => {
+          const filename = payload.image.name
+          const ext = filename.slice(filename.lastIndexOf('.'))
+          return firebase.storage().ref(`meetups/${key}.${ext}`).put(payload.image)
+        })
+        .then(fileData => {
+          imageUrl = fileData.metadata.downloadURLs[0]
+          return firebase.database().ref('meetups').child(key).update({imageUrl: imageUrl})
+        })
+        .then(() => {
+          commit('createMeetup', {
+            ...meetup,
+            imageUrl: imageUrl,
+            id: key
+          })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    updateMeetupData ({commit}, payload) {
+      commit('setLoading', true)
+      const updateObj = {}
+      if (payload.title) {
+        updateObj.title = payload.title
+      }
+      if (payload.description) {
+        updateObj.description = payload.description
+      }
+      if (payload.date) {
+        updateObj.date = payload.date
+      }
+      firebase.database().ref('meetups').child(payload.id).update(updateObj)
+        .then(() => {
+          commit('setLoading', false)
+          commit('updateMeetup', payload)
+        })
+        .catch(err => {
+          console.log(err)
+          commit('setLoading', false)
+        })
+    }
+  },
+  getters: {
+    loadedMeetups (state) {
+      return state.loadedMeetups.sort((meetupA, meetupB) => {
+        return meetupA.date > meetupB.date
+      })
+    },
+    loadedMeetup (state) {
+      return (meetupId) => {
+        return state.loadedMeetups.find((meetup) => {
+          return meetup.id === meetupId
+        })
+      }
+    },
+    featuredMeetups (state, getters) {
+      return getters.loadedMeetups.slice(0, 5)
+    }
+  }
+}
